3e6f50dfb673491f0e52b5eb759227507e55b4fe
KKKKit123 <suwasin.kit.bbv@gmail.com>
2021-02-06 22:42:23 +0700

    Add File

---
+++ while_for_exercise1.py
@@ -0,0 +1,21 @@
+#while
+i = 2
+while i <= 12:
+    j = 1
+    print("  [", i, "]")
+    while j <= 12:
+        print(i, "*", j, ":", i * j, end="\n")
+        j += 1
+    print("---------------")
+    i += 1
+
+#for
+i = 2
+for i in range(2, 13):
+    j = 1
+    print("  [", i, "]")
+    for j in range(2, 13):
+        print(i, "*", j, ":", i * j, end="\n")
+        j += 1
+    print("---------------")
+    i += 1
